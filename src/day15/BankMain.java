d6423f44be71da14fd16aea16825fca94850d2b6
sustudios <sustudios@sustudios-PC>
2021-04-19 12:47:51 +0900

    day15 x class.!!!

---
+++ src/day15/BankMain.java
@@ -0,0 +1,37 @@
+package day15;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankMain {
+
+	public static void main(String[] args) {
+		
+		
+		
+
+    Bank b1 =new Bank ();
+ b1.
+	
+	List<String>banklist=new ArrayList<String>();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
